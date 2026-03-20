34483afd98d8215f409e2429d879d0ed2fd830ae
unknown <wisdomtoyin@gmail.com>
2026-03-20 21:27:30 +0000

    Fix auth route prisma import

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,14 +1,14 @@
 import { PrismaClient } from "@prisma/client"
 
-declare global {
-  var prisma: PrismaClient | undefined
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
 }
 
-const prisma = global.prisma || new PrismaClient()
+const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma
+  globalForPrisma.prisma = prisma
 }
 
-export { prisma }
 export default prisma
+export { prisma }
